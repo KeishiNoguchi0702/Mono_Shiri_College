7c810555b785677b1dfc4f2c307d0ec280f287b4
KeishiNoguchi0702 <thomastyaso1127@gmail.com>
2021-06-09 18:06:01 +0900

    Userモデルのカラムを定義

--- db/migrate/20210609090355_devise_create_users.rb
+++ db/migrate/20210609090355_devise_create_users.rb
@@ -4,8 +4,17 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
+      t.string  :certificate_number, null: false, uniqueness: true
+      t.string  :email,              null: false, default: ""
+      t.string  :encrypted_password, null: false, default: ""
+      t.string  :last_name,          null: false
+      t.string  :first_name,         null: false
+      t.string  :last_name_kana,     null: false
+      t.string  :first_name_kana,    null: false
+      t.integer :department_id,      null: false
+      t.integer :sex_id,             null: false
+      t.integer :position_id,        null: false
+      t.text    :profile
 
       ## Recoverable
       t.string   :reset_password_token
