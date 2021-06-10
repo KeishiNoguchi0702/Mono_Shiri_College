75d91ef0c951e6651dbd1942dee97c8d48d531c5
KeishiNoguchi0702 <thomastyaso1127@gmail.com>
2021-06-10 11:15:46 +0900

    バリデーションを記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,20 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :certificate_number, uniqueness: true
+    validates :last_name
+    validates :first_name
+    validates :last_name_kana
+    validates :first_name_kana
+    # アクティブハッシュ使用欄は初期値以外の選択を強制
+    with_options numericality: { greater_than_equal_to: 1 } do
+      validates :department_id
+      validates :sex_id
+      validates :position_id
+    end
+  end
   
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :department
