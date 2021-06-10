e64a023478a88813c9685289054d639020f0e903
KeishiNoguchi0702 <thomastyaso1127@gmail.com>
2021-06-10 21:42:20 +0900

    UserのFactoryBotを定義

---
+++ spec/factories/users.rb
@@ -0,0 +1,22 @@
+FactoryBot.define do
+  
+  factory :user do
+    certificate_number      { Faker::Number.number(digits: 6) }
+    email                   { Faker::Internet.email }
+    password                { '1a' + Faker::Internet.password(min_length: 6) }
+    password_confirmation   {password}
+    last_name               { '保家' }
+    first_name              { '太郎' }
+    last_name_kana          { 'ホゲ' }
+    first_name_kana         { 'タロウ' }
+    department_id           { 1 }
+    sex_id                  { 1 }
+    position_id             { 1 }
+    profile                 { Faker::Lorem.word }
+
+    after(:build) do |user|
+      user.image.attach(io: File.open('app/assets/spectest/ok.png'), filename: 'ok.png')
+    end
+
+  end
+end
