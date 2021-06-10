baeab8d6e98a117acac7ae112bd4acb4706232ab
KeishiNoguchi0702 <thomastyaso1127@gmail.com>
2021-06-10 16:06:09 +0900

    生成した要素をブラウザに表示

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -1,5 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
+
   const ImageList = document.getElementById('image_list');
+
   document.getElementById('user-image').addEventListener('change', function(e){
     const file = e.target.files[0];
     const blob = window.URL.createObjectURL(file);
@@ -7,5 +9,9 @@ document.addEventListener('DOMContentLoaded', function() {
     const imageElement = document.createElement('div');
     const blobImage = document.createElement('img');
     blobImage.setAttribute('src', blob)
+
+    imageElement.appendChild(blobImage);
+    ImageList.appendChild(imageElement);
   });
+
 });
