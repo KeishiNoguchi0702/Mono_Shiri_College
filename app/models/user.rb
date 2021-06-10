f31c60d0c99ecf3930e85cea6d61868bf61124e9
KeishiNoguchi0702 <thomastyaso1127@gmail.com>
2021-06-10 17:52:09 +0900

    バリデーションを追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,13 +5,23 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   with_options presence: true do
-    validates :certificate_number, uniqueness: true
-    validates :last_name
-    validates :first_name
-    validates :last_name_kana
-    validates :first_name_kana
-    # アクティブハッシュ使用欄は初期値以外の選択を強制
-    with_options numericality: { greater_than_equal_to: 1 } do
+    #在籍番号（数字6桁）
+    validates :certificate_number, uniqueness: true, numericality: { only_integer: true }, length: { minimum: 6 }
+
+    #氏名入力欄（全角ひらがな・カタカナ・漢字）
+    with_options format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/, message: 'is invalid. Input full-width characters.' } do
+      validates :last_name
+      validates :first_name
+    end
+
+    #カナ入力（全角カタカナ）
+    with_options format: { with: /\A[ァ-ヶー－]+\z/, message: 'is invalid. Input full-width katakana characters.' } do
+      validates :last_name_kana
+      validates :first_name_kana
+    end
+
+    #学部学科、性別、在籍区分（初期値以外の選択必須）
+    with_options numericality: { greater_than_or_equal_to: 1 } do
       validates :department_id
       validates :sex_id
       validates :position_id
