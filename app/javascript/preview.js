100aa4b6e4eebf04c1e65bf65ef593c94a8be8af
KeishiNoguchi0702 <thomastyaso1127@gmail.com>
2021-06-10 16:18:36 +0900

    別画面でコンソールエラーが表示されない機能を実装

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -1,17 +1,19 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-  const ImageList = document.getElementById('image_list');
+  if ( document.getElementById('user-image')) {
+    const ImageList = document.getElementById('image_list');
 
-  document.getElementById('user-image').addEventListener('change', function(e){
-    const file = e.target.files[0];
-    const blob = window.URL.createObjectURL(file);
+    document.getElementById('user-image').addEventListener('change', function(e){
+      const file = e.target.files[0];
+      const blob = window.URL.createObjectURL(file);
 
-    const imageElement = document.createElement('div');
-    const blobImage = document.createElement('img');
-    blobImage.setAttribute('src', blob)
+      const imageElement = document.createElement('div');
+      const blobImage = document.createElement('img');
+      blobImage.setAttribute('src', blob);
 
-    imageElement.appendChild(blobImage);
-    ImageList.appendChild(imageElement);
-  });
-
+      imageElement.appendChild(blobImage);
+      ImageList.appendChild(imageElement);
+    });
+  };
+  
 });
