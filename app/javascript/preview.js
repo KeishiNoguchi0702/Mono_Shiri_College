8b8b64377774397ffdeac4a1a69e2a3c4f993a17
KeishiNoguchi0702 <thomastyaso1127@gmail.com>
2021-06-10 15:59:26 +0900

    画像情報を変数に格納

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
   const ImageList = document.getElementById('image_list');
   document.getElementById('user-image').addEventListener('change', function(e){
-    console.log(e);
+    const file = e.target.files[0];
   });
 });
