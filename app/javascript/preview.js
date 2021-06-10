f6cfe205a52108fe516417b34dde87bbff507d62
KeishiNoguchi0702 <thomastyaso1127@gmail.com>
2021-06-10 16:01:04 +0900

    取得した画像のURLを生成

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -2,5 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
   const ImageList = document.getElementById('image_list');
   document.getElementById('user-image').addEventListener('change', function(e){
     const file = e.target.files[0];
+    const blob = window.URL.createObjectURL(file);
+    
   });
 });
