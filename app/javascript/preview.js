4c4699951a190a8aa934f244dd11b4d1be1f38ba
KeishiNoguchi0702 <thomastyaso1127@gmail.com>
2021-06-11 21:39:13 +0900

    画像プレビューをリファクタリング

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -1,19 +1,30 @@
 document.addEventListener('DOMContentLoaded', function() {
-
   if ( document.getElementById('user-image')) {
     const ImageList = document.getElementById('image_list');
 
-    document.getElementById('user-image').addEventListener('change', function(e){
-      const file = e.target.files[0];
-      const blob = window.URL.createObjectURL(file);
-
+    const createImageHTML = (blob) => {
+      //画像を表示するためのdiv要素を生成
       const imageElement = document.createElement('div');
+
+      //表示する画像を生成
       const blobImage = document.createElement('img');
       blobImage.setAttribute('src', blob);
 
+      //生成したHTML要素をブラウザに表示
       imageElement.appendChild(blobImage);
       ImageList.appendChild(imageElement);
+    };
+
+    document.getElementById('user-image').addEventListener('change', function(e){
+      //画像が表示されている場合は既存画像を削除
+      const imageContent = document.querySelector('img');
+      if (imageContent) {
+        imageContent.remove();
+      };
+
+      const file = e.target.files[0];
+      const blob = window.URL.createObjectURL(file);
+      createImageHTML(blob);
     });
-  };
-  
+  }
 });
