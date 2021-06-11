e667833b112b1ac3c032cb5112b63474f9bd7614
KeishiNoguchi0702 <thomastyaso1127@gmail.com>
2021-06-11 19:30:51 +0900

    バリデーションをアップデート

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,10 +8,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
+
   with_options presence: true do
     #メールアドレス
-    #validates :email, format: { with: /\A[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*\z/,
-    #                            message: "のドメインは使用できません。使用できるドメインの一覧は、【使用可能なドメイン】を参照ください。" }
+    validates :email, format: { with: /\A[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*\z/ }
 
     #氏名入力欄（全角ひらがな・カタカナ・漢字）
     with_options format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/, message: 'には全角ひらがな・カタカナ・漢字以外は入力できません' } do
@@ -26,17 +28,14 @@ class User < ApplicationRecord
     end
 
     #学部学科、性別、在籍区分（初期値以外の選択必須）
-    with_options format: { with: /[1-9]|[1-9][0-9]/, message: 'を選択してください' } do
-      validates :department_id
-      validates :sex_id
-      validates :position_id
-    end
+    validates :department_id, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 24, message: 'を正しく選択してください' }
+    validates :sex_id,        numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 2 , message: 'を正しく選択してください' }
+    validates :position_id,   numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 3 , message: 'を正しく選択してください' }
   end
 
   #アップロード可能な画像を指定（ファイル形式、サイズ、縦幅・横幅）
   validates :image, content_type: [:png, :jpg, :jpeg],
-                    size: { less_than_or_equal_to: 10.megabytes },
-                    dimension: { width: { max: 1500} , height: { max: 1500 } }
+                    size: { less_than_or_equal_to: 10.megabytes }
 
   #active_storageとのアソシエーション
   has_one_attached :image
