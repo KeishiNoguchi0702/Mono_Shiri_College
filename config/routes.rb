0bec9ad57b9a0d52edb08fa35d7feb97cc366ea3
KeishiNoguchi0702 <thomastyaso1127@gmail.com>
2021-06-14 17:18:23 +0900

    User別の詳細画面表示用にルーティングをネスト

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/show'
-  devise_for :users
   resources :users, only: [:show]
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
+  
   resources :aritcles
   root 'articles#index'
 end
