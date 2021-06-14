fc806b3052bcff1d1b74f10866ee619bda056658
KeishiNoguchi0702 <thomastyaso1127@gmail.com>
2021-06-14 17:04:03 +0900

    改行を追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,13 @@ class ApplicationController < ActionController::Base
   private
 
   def configure_permitted_parameters
+    
     devise_parameter_sanitizer.permit(:sign_up,
       keys: [:certificate_number, :last_name, :first_name, :last_name_kana, :first_name_kana, :department_id, :sex_id, :position_id, :profile, :image])
-    devise_parameter_sanitizer.permit(:account_update,
+    
+      devise_parameter_sanitizer.permit(:account_update,
       keys: [:certificate_number, :last_name, :first_name, :last_name_kana, :first_name_kana, :department_id, :sex_id, :position_id, :profile, :image])
-  end
+  
+    end
 
 end
