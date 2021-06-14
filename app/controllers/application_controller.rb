a852fde10a146d3555d96ad545602dbc28803aa9
KeishiNoguchi0702 <thomastyaso1127@gmail.com>
2021-06-14 20:48:56 +0900

    インデントを修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up,
       keys: [:certificate_number, :last_name, :first_name, :last_name_kana, :first_name_kana, :department_id, :sex_id, :position_id, :profile, :image])
     
-      devise_parameter_sanitizer.permit(:account_update,
+    devise_parameter_sanitizer.permit(:account_update,
       keys: [:certificate_number, :last_name, :first_name, :last_name_kana, :first_name_kana, :department_id, :sex_id, :position_id, :profile, :image])
   
     end
