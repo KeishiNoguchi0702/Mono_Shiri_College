0560d9de21b56d3e9bc1a8a6acc76706844fbff8
KeishiNoguchi0702 <thomastyaso1127@gmail.com>
2021-06-14 17:08:37 +0900

    CSRF対策

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,11 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
 
   def configure_permitted_parameters
-    
+
     devise_parameter_sanitizer.permit(:sign_up,
       keys: [:certificate_number, :last_name, :first_name, :last_name_kana, :first_name_kana, :department_id, :sex_id, :position_id, :profile, :image])
     
