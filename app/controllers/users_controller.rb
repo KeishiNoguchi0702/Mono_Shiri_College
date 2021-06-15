26e0c99682b6f504ef726ffa28466c99f6551b14
KeishiNoguchi0702 <thomastyaso1127@gmail.com>
2021-06-15 22:33:06 +0900

    usersコントローラーにセキュリティ強化のbefore_actionを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
   before_action :user_set
-
+  before_action :move_to_index, except: :show
+  
   def show
   end
 
@@ -12,6 +14,10 @@ class UsersController < ApplicationController
 
   def user_set
     @user = User.find(params[:id])
+  end
+
+  def move_to_index
+    redirect_to root_path if current_user.id != @user.id
   end
 
 end
