43d78c70906489625ac3b7c92ee4810d17ae86f8
KeishiNoguchi0702 <thomastyaso1127@gmail.com>
2021-06-15 22:06:42 +0900

    destroyアクションを定義

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,16 @@
 class UsersController < ApplicationController
+  before_action :user_set
 
   def show
+  end
+
+  def destroy
+    render template: "retirements/done" if @user.destroy
+  end
+
+  private
+
+  def user_set
     @user = User.find(params[:id])
   end
 
