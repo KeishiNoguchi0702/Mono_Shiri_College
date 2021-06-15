6df6b608669f07a0da1220a09be414641e48dd86
KeishiNoguchi0702 <thomastyaso1127@gmail.com>
2021-06-15 11:29:30 +0900

    パスワードレビューJSを修正
    パスワード入力欄が存在しない画面でコンソールエラーが発生しないようにするため

--- app/javascript/password_view.js
+++ app/javascript/password_view.js
@@ -1,29 +1,29 @@
 document.addEventListener('DOMContentLoaded', function(){
- 
-  //パスワードオブジェクトの取得
-  const pwd1 = document.getElementById('password');
-  const pwdCheck1 = document.getElementById('password_checkbox_1');
+  if ( document.getElementById('password') ) {
+    //パスワードオブジェクトの取得
+    const pwd1 = document.getElementById('password');
+    const pwdCheck1 = document.getElementById('password_checkbox_1');
 
-  //パスワードの表示
-  pwdCheck1.addEventListener('change', function() {
-    if(pwdCheck1.checked) {
-      pwd1.setAttribute('type', 'text');
-    } else {
-      pwd1.setAttribute('type', 'password');
-    }
-  });
+    //パスワードの表示
+    pwdCheck1.addEventListener('change', function() {
+      if(pwdCheck1.checked) {
+        pwd1.setAttribute('type', 'text');
+      } else {
+        pwd1.setAttribute('type', 'password');
+      }
+    });
 
-  //パスワードオブジェクトの取得
-  const pwd2 = document.getElementById('password-confirmation');
-  const pwdCheck2 = document.getElementById('password_checkbox_2');
+    //パスワードオブジェクトの取得
+    const pwd2 = document.getElementById('password-confirmation');
+    const pwdCheck2 = document.getElementById('password_checkbox_2');
 
-  //パスワード（確認用）の表示
-  pwdCheck2.addEventListener('change', function() {
-    if(pwdCheck2.checked) {
-      pwd2.setAttribute('type', 'text');
-    } else {
-      pwd2.setAttribute('type', 'password');
-    }
-  });
-
+    //パスワード（確認用）の表示
+    pwdCheck2.addEventListener('change', function() {
+      if(pwdCheck2.checked) {
+        pwd2.setAttribute('type', 'text');
+      } else {
+        pwd2.setAttribute('type', 'password');
+      }
+    });
+  }
 });
