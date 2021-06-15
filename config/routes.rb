c188878c5ed1dc38e3323ce6c8fcbf425a331fc9
KeishiNoguchi0702 <thomastyaso1127@gmail.com>
2021-06-15 17:57:11 +0900

    退会機能のルーティングを定義

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
   root to: 'articles#index'
   devise_for :users
-  #devise_for :users, :controllers => {
-  #  :sessions => "users/sessions",
-  #  :registrations => "users/registrations"
-  #}
   resources :users, only: [:show]
   resources :aritcles
+  resources :retirements, only: [:show, :create]
 end
