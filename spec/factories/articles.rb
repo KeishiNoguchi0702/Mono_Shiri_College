16135fd094a3d1dd9fd1c57e9cdadf2a599cc5c2
KeishiNoguchi0702 <thomastyaso1127@gmail.com>
2021-06-17 21:10:19 +0900

    articleモデルの単体テストを記述

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :article do
-    
+    title { Faker::Lorem.word }
+    content { Faker::Lorem.word }
+    user
   end
 end
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before do
+    @article = FactoryBot.build(:article)
+  end
+
+  describe '新規記事投稿' do
+    context '記事投稿できるとき' do
+      it '妥当なオブジェクト' do
+        expect(@article).to be_valid
+      end
+    end
+
+    context '記事投稿できないとき' do
+      it 'タイトルが空では登録できない' do
+        @article.title = ''
+        expect(@article).not_to be_valid
+      end
+
+      it '投稿内容が空では登録できない' do
+        @article.content = ''
+        expect(@article).not_to be_valid
+      end
+
+      it '投稿内容が空では登録できない' do
+        @article.user = nil
+        expect(@article).not_to be_valid
+      end
+    end
+  end
+
 end
