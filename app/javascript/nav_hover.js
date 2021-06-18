99512d7b71033411707ff51449acf0b14c35f154
KeishiNoguchi0702 <thomastyaso1127@gmail.com>
2021-06-19 05:53:45 +0900

    ナビゲーションバーのホバーアクションを追加

--- app/javascript/nav_hover.js
+++ app/javascript/nav_hover.js
@@ -1,3 +1,47 @@
 function navHover() {
-  console.log('ok');
+  
+  if ( document.getElementById('site_gnav_list') ) {
+
+    //TOPページボタン
+    const navTopBtn = document.getElementById('top_btn');
+    navTopBtn.addEventListener('mouseover', function() {
+      this.setAttribute("style", "text-decoration: underline;");
+    });
+
+    navTopBtn.addEventListener('mouseout', function() {
+      this.setAttribute("style", "text-decoration: none;");
+    });
+
+    //NEWSボタン
+    const navNewsBtn = document.getElementById('news_btn');
+    navNewsBtn.addEventListener('mouseover', function() {
+      this.setAttribute("style", "text-decoration: underline;");
+    });
+
+    navNewsBtn.addEventListener('mouseout', function() {
+      this.setAttribute("style", "text-decoration: none;");
+    });
+
+    //ARTICLESボタン
+    const navArtclesBtn = document.getElementById('articles_btn');
+    navArtclesBtn.addEventListener('mouseover', function() {
+      this.setAttribute("style", "text-decoration: underline;");
+    });
+
+    navArtclesBtn.addEventListener('mouseout', function() {
+      this.setAttribute("style", "text-decoration: none;");
+    });
+
+    //USERSボタン
+    const navUsersBtn = document.getElementById('users_btn');
+    navUsersBtn.addEventListener('mouseover', function() {
+      this.setAttribute("style", "text-decoration: underline;");
+    });
+
+    navUsersBtn.addEventListener('mouseout', function() {
+      this.setAttribute("style", "text-decoration: none;");
+    });
+  };
 };
+
+window.addEventListener('load', navHover);
