f80a14e8315cb9fc31384f6893ef00555dc89c6f
KeishiNoguchi0702 <thomastyaso1127@gmail.com>
2021-06-20 18:29:10 +0900

    途中

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,6 +12,7 @@ require('../password_view')
 require('../move_to_top')
 require('../nav_hover')
 require('../new_articles_hover')
+require('../drop_down.js')
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
