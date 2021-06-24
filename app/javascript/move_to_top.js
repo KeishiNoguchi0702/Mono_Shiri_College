fdf0e3e2a3237dd8ad633dcf8fb1f8c1792b486d
KeishiNoguchi0702 <thomastyaso1127@gmail.com>
2021-06-24 22:33:55 +0900

    退会完了後の遷移先を本番環境に修正

--- app/javascript/move_to_top.js
+++ app/javascript/move_to_top.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
   if ( document.getElementById('retirement_done') ) {
     setTimeout(function() {
-      window.location.href = 'http://localhost:3000/';
+      window.location.href = 'http://13.230.22.136/';
     }, 6 * 1000);
   }
 
