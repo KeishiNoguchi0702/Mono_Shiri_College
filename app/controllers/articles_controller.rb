07a1df2d6aace5c1eaa23b40051494550f2d390c
KeishiNoguchi0702 <thomastyaso1127@gmail.com>
2021-06-27 22:40:56 +0900

    背景動画表示のため調整

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,7 +4,8 @@ class ArticlesController < ApplicationController
   before_action :article_new, only: [:new, :update]
 
   def index
-    @articles = Article.includes([:rich_text_content]).order("created_at DESC").page(params[:page]).per(15)
+    @articles = Article.order("created_at DESC").page(params[:page]).per(15)
+    #@articles = Article.includes([:rich_text_content]).order("created_at DESC").page(params[:page]).per(15)
   end
 
   def show
