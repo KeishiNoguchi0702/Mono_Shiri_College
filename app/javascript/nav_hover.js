a0efe902ae12060cc111fd952c4a82ba1cb3c66f
KeishiNoguchi0702 <thomastyaso1127@gmail.com>
2021-07-01 22:44:13 +0900

    ナビゲーションのホバー機能をリファクタリング

--- app/javascript/nav_hover.js
+++ app/javascript/nav_hover.js
@@ -1,47 +1,73 @@
 function navHover() {
-  
-  if ( document.getElementById('site_gnav_list') ) {
 
-    //TOPページボタン
-    const navTopBtn = document.getElementById('top_btn');
-    navTopBtn.addEventListener('mouseover', function() {
-      this.setAttribute("style", "text-decoration: underline;");
-    });
+  const btn_arr = ['top_btn',
+                   'news_btn',
+                   'articles_btn',
+                   'users_btn',
+                   'sign_in',
+                   'sign_up'];
 
-    navTopBtn.addEventListener('mouseout', function() {
-      this.setAttribute("style", "text-decoration: none;");
-    });
+  for (const elem of btn_arr) {
+    if ( document.getElementById(elem) ) {
+      const navBtn = document.getElementById(elem);
 
-    //NEWSボタン
-    const navNewsBtn = document.getElementById('news_btn');
-    navNewsBtn.addEventListener('mouseover', function() {
-      this.setAttribute("style", "text-decoration: underline;");
-    });
+      navBtn.addEventListener('mouseover', function() {
+        this.setAttribute("style", "text-decoration: underline;");
+      });
 
-    navNewsBtn.addEventListener('mouseout', function() {
-      this.setAttribute("style", "text-decoration: none;");
-    });
-
-    //ARTICLESボタン
-    const navArtclesBtn = document.getElementById('articles_btn');
-    navArtclesBtn.addEventListener('mouseover', function() {
-      this.setAttribute("style", "text-decoration: underline;");
-    });
-
-    navArtclesBtn.addEventListener('mouseout', function() {
-      this.setAttribute("style", "text-decoration: none;");
-    });
-
-    //USERSボタン
-    const navUsersBtn = document.getElementById('users_btn');
-    navUsersBtn.addEventListener('mouseover', function() {
-      this.setAttribute("style", "text-decoration: underline;");
-    });
-
-    navUsersBtn.addEventListener('mouseout', function() {
-      this.setAttribute("style", "text-decoration: none;");
-    });
+      navBtn.addEventListener('mouseout', function() {
+        this.setAttribute("style", "text-decoration: none;");
+      });
+    };
   };
+
 };
 
 window.addEventListener('load', navHover);
+
+//function navHover() {
+//  if ( document.getElementById('site_gnav_list') ) {
+
+//    //TOPページボタン
+//    const navTopBtn = document.getElementById('top_btn');
+//    navTopBtn.addEventListener('mouseover', function() {
+//      this.setAttribute("style", "text-decoration: underline;");
+//    });
+
+//    navTopBtn.addEventListener('mouseout', function() {
+//      this.setAttribute("style", "text-decoration: none;");
+//    });
+
+//    //NEWSボタン
+//    const navNewsBtn = document.getElementById('news_btn');
+//    navNewsBtn.addEventListener('mouseover', function() {
+//      this.setAttribute("style", "text-decoration: underline;");
+//    });
+
+//    navNewsBtn.addEventListener('mouseout', function() {
+//      this.setAttribute("style", "text-decoration: none;");
+//    });
+
+//    //ARTICLESボタン
+//    const navArtclesBtn = document.getElementById('articles_btn');
+//    navArtclesBtn.addEventListener('mouseover', function() {
+//      this.setAttribute("style", "text-decoration: underline;");
+//    });
+
+//    navArtclesBtn.addEventListener('mouseout', function() {
+//      this.setAttribute("style", "text-decoration: none;");
+//    });
+
+//    //USERSボタン
+//    const navUsersBtn = document.getElementById('users_btn');
+//    navUsersBtn.addEventListener('mouseover', function() {
+//      this.setAttribute("style", "text-decoration: underline;");
+//    });
+
+//    navUsersBtn.addEventListener('mouseout', function() {
+//      this.setAttribute("style", "text-decoration: none;");
+//    });
+//  };
+//};
+
+//window.addEventListener('load', navHover);
