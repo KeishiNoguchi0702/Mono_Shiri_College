f2e1c6c0774c3f6834e432f392482be0f5e68975
KeishiNoguchi0702 <thomastyaso1127@gmail.com>
2021-07-02 21:29:55 +0900

    記事更新画面をリファクタリング

--- app/javascript/target_bottun_hover.js
+++ app/javascript/target_bottun_hover.js
@@ -5,6 +5,7 @@ function targetButtonHover() {
                'article_user_top_link',
                'article_user_articles_link',
                'post_article_btn',
+               'update_article_btn',
                'register-red-btn',
                'user_sign_in_btn',
                'user_viewer_signout_btn',
