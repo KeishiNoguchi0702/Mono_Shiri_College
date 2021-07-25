568daba915e33292eb73b42fb29225cbe5b011b2
KeishiNoguchi0702 <thomastyaso1127@gmail.com>
2021-07-25 22:35:38 +0900

    コメント追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
     @users = User.with_attached_image.order('certificate_number ASC').page(params[:page]).per(20)
   end
 
+  #ユーザ詳細表示
   def show
     @user = User.find(params[:id])
   end
